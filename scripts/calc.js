d2719ca3ddb52f7037d66ea7b8f512b5ce8ae9d0
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-07-30 18:24:17 +0000

    feat: refactor our test a bit

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,11 @@
-function addition() {
-    return 50
+function addition(numOne=0, numTwo=0) {
+    if(typeof numOne === "string" || typeof numTwo === "string") {
+        return "Parameter should be type of integer";
+    };
+    if(typeof numOne === "object" || typeof numTwo === "object") {
+        return "Can not add null values";
+    };
+    return numOne + numTwo;
 };
 
 
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,6 +5,42 @@ describe("calculator", () => {
         test("should return 50 for 25 + 25", () => {
             expect(addition(25, 25)).toBe(50);
         });
+        test("should return 65 for 35 + 39", () => {
+            expect(addition(35, 30)).toBe(65);
+        });
+        test("should return 0 for no parameter", () => {
+            expect(addition()).toBe(0);
+        });
+        test("should return 'Parameter should be typeof integer' for string type", () => {
+            expect(addition("1", "23")).toBe("Parameter should be type of integer");
+        });
+        test("should return 'Parameter should be typeof integer' for string type", () => {
+            expect(addition("1", 1)).toBe("Parameter should be type of integer");
+        });
+        test("should return 'Parameter should be typeof integer' for string type", () => {
+            expect(addition(23, "34")).toBe("Parameter should be type of integer");
+        });
+        test("should return -15 for -5 + -10 ", () => {
+            expect(addition(-5, -10)).toBe(-15);
+        });
+        test("should return 5 for -5 + 10 ", () => {
+            expect(addition(-5, 10)).toBe(5);
+        });
+        test("should return 5 for 10 + -5 ", () => {
+            expect(addition(10, -5)).toBe(5);
+        });
+        test("should return 5 for 5 + no parameter", () => {
+            expect(addition(5)).toBe(5);
+        });
+        test("should return 'Can not add null values' for null parameters", () => {
+            expect(addition(5, null)).toBe("Can not add null values");
+        });
+        test("should return 'Can not add null values' for null parameters", () => {
+            expect(addition(null, null)).toBe("Can not add null values");
+        });
+        test("should return 'Can not add null values' for null parameters", () => {
+            expect(addition(null, 5)).toBe("Can not add null values");
+        });
 
     });
 });
